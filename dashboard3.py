121576be0426c2c5f64e796d0279fd5f4b3e4a68
rastudillon <137467194+rastudillon@users.noreply.github.com>
2023-07-20 00:39:12 -0400

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -403,6 +403,199 @@ def dashboard_personalizado(df):
         pass
         graf_pie_campus_dashboard(filtro)
 
+def pie_valores_nulos(df):
+    fig = px.pie(df.isnull().sum(),values=df.isnull().sum().values,names=df.isnull().sum().index.tolist(),hole=0.3)     
+    st.plotly_chart(fig,use_container_width=True)
+
+def bar_cantidad_nulos(df):
+    fig = px.bar(df.isnull().sum(),x=df.isnull().sum().values,y=df.isnull().sum().index.tolist())      
+    st.plotly_chart(fig,use_container_width=True)
+
+def distribucion_col_num(df):
+    df = df.drop("Adj.", axis=1)
+    df = df.drop("Rut Responsable_dsc",axis=1)
+    size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+    title = "Distribución de columnas numéricas"
+    st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True)
+    for col in df.select_dtypes(include=np.number):
+        fig = px.histogram(df, x=col)
+        fig.update_layout(title={
+                                "text":f"Distribución de {col}",
+                                "x":0.5,
+                                "xanchor": "center"},
+                      title_font_color= "#D8E2DC")       
+        st.plotly_chart(fig,use_container_width=True)
+
+def distribucion_col_categoricas(df):
+    df = df.drop(["Resumen","Detalle","Ubicación","Anexo","Funcionario de Contacto_cod","Funcionario de Contacto_cod",
+                  "Funcionario de Contacto_dsc","Fecha de Recepción","Funcionario Encargado_cod","Funcionario Encargado_dsc",
+                  "Funcionario Ejecutor_cod","Fecha de Término","Observación","Material Utilizado","Rut Responsable_cod","Rut Responsable_dsc"],axis=1)
+   
+    size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+    title = "Frecuencia de columnas categóricas"
+    st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True)
+    for col in df.select_dtypes(include=object):
+        fig = px.histogram(df, x=col)
+        fig.update_layout(title={
+                                "text":f"Frecuencia de {col}",
+                                "x":0.5,
+                                "xanchor": "center"},
+                      title_font_color= "#D8E2DC") 
+        st.plotly_chart(fig,use_container_width=True) 
+
+def analisis_exp(df):
+    size_title = 'font-size: 40px; text-align: center; color: #D8E2DC; font-weight: lighter'
+    title = "Análisis explotario de la base de datos ingresada"
+    st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True) 
+    size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+    title = "Cabecera de la base de datos"
+    st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True) 
+    st.write(df.head(5))
+    size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+    title = "Descripción estadística de la base de datos"
+    st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True) 
+    st.write(df.describe())
+
+    c1,c2 = st.columns(2)
+    with c1:
+        size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+        title = "Porcentaje de valores nulos por columna"
+        st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True) 
+        pie_valores_nulos(df)
+    with c2:
+        size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+        title = "Cantidad de valores nulos por columna"
+        st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True)        
+        bar_cantidad_nulos(df)
+    c1,c2 = st.columns(2)
+    with c1:
+        distribucion_col_num(df)
+        size_title = 'font-size: 30px; text-align: center; color: #D8E2DC; font-weight: lighter'
+        title = "Tipos de datos por columna"
+        st.write(f'<p style="{size_title}">{title}</p>',unsafe_allow_html=True)       
+        st.dataframe(df.dtypes)
+    with c2:
+        distribucion_col_categoricas(df)
+
+def grafico_barras(df):
+
+    df.drop(columns=["Adj.","Fecha de Recepción","Funcionario Encargado_cod","Funcionario Encargado_dsc","Fecha de Asignación","Funcionario Ejecutor_cod","Funcionario Ejecutor_dsc","Solicitud de Compra","Observación","Cantidad de Personas Involucradas","Nº de Horas Hombre","Material Utilizado","Rut Responsable_dsc"],inplace=True)
+    
+
+    st.title('Generador de Gráficos de Barras')
+    valores_columnas = ['Centro de Costo_dsc', 'Nº de Solicitud']
+   
+
+    x_col = st.selectbox('Selecciona la columna para el eje X', options=df.columns)
+    val_col = st.selectbox('Selecciona la columna para el eje X', options=valores_columnas)
+
+ 
+    if st.button('Generar Gráfico de Barras'):
+       
+        # Contar los valores de la columna seleccionada y obtener los 10 más importantes
+        counts = df[x_col].value_counts().nlargest(10).reset_index()
+        counts.columns = [x_col, val_col]
+
+        # Generar el gráfico de líneas
+        st.markdown(f"## Gráfico de barras de {x_col}")
+        fig = px.bar(counts, x=x_col, y=val_col,  labels={'index': x_col}, title=f'Gráfico de barras de {x_col}')
+        
+        st.plotly_chart(fig)
+
+def grafico_lineas(df):
+
+    df.drop(columns=["Adj.","Fecha de Recepción","Funcionario Encargado_cod","Funcionario Encargado_dsc","Fecha de Asignación","Funcionario Ejecutor_cod","Funcionario Ejecutor_dsc","Solicitud de Compra","Observación","Cantidad de Personas Involucradas","Nº de Horas Hombre","Material Utilizado","Rut Responsable_dsc"],inplace=True)
+    
+
+    st.title('Generador de Gráficos de Líneas')
+
+    valores_columnas = ['Centro de Costo_dsc', 'Nº de Solicitud']
+   
+
+    x_col = st.selectbox('Selecciona la columna para el eje X', options=df.columns)
+    val_col = st.selectbox('Selecciona la columna para el eje X', options=valores_columnas)
+
+    # Contar los valores de la columna seleccionada y obtener los 10 más importantes
+    counts = df[x_col].value_counts().nlargest(10).reset_index()
+    counts.columns = [x_col, val_col]
+
+    # Generar el gráfico de líneas
+    fig = px.line(counts, x=x_col, y=val_col, title='Gráfico de Líneas')
+    st.plotly_chart(fig)
+
+def grafico_boxplot(df):
+    df.drop(columns=["Adj.","Fecha de Recepción","Funcionario Encargado_cod","Funcionario Encargado_dsc","Fecha de Asignación","Funcionario Ejecutor_cod","Funcionario Ejecutor_dsc","Solicitud de Compra","Observación","Material Utilizado","Rut Responsable_dsc"],inplace=True)
+    
+
+    st.title('Generador de Gráficos de Líneas')
+    valores_columnas = ['Centro de Costo_dsc', 'Nº de Solicitud']
+    groupby_columna=['Ubicación del Trabajo/Servicio','Tipo de Trabajo','Nº de Horas Hombre','Cantidad de Personas Involucradas']
+    
+    # Solicitar al usuario el valor de la columna y la columna para agrupar
+    valor_columna = st.selectbox("Selecciona la columna de valor", valores_columnas)
+    groupby_x = st.selectbox("Selecciona la columna para agrupar", groupby_columna)
+    # Generar el gráfico de cajas utilizando plotly.express
+    fig = px.box(df, y=valor_columna, x=groupby_x)
+
+    # Mostrar el gráfico utilizando Streamlit
+    st.plotly_chart(fig)
+
+def grafico_histograma(df):
+    st.title('Generador de Gráficos de Histograma')
+
+    columnas = ['Nº de Solicitud', 'Fecha', 'Tipo de Servicio', 'Nº de Horas Hombre', 
+                'Cantidad de Personas Involucradas', 'Material Utilizado', 'Ubicación del Trabajo/Servicio']
+
+    # Seleccionar una columna para el gráfico de histograma
+    columna_graficar = st.selectbox('Elige una columna para generar el histograma', options=columnas)
+
+    # Filtrar valores nulos en la columna seleccionada
+    df_filtrado = df[df[columna_graficar].notnull()]
+
+    # Generar el gráfico de histograma
+    fig = px.histogram(df_filtrado, x=columna_graficar, title=f'Histograma de {columna_graficar}')
+
+    # Mostrar el gráfico
+    st.plotly_chart(fig)
+
+def grafico_pastel(df):
+    st.title('Generador de Gráfico de Pastel')
+
+    # Definir las columnas adecuadas para el gráfico de pastel
+    columnas = ['Tipo de Servicio', 'Centro de Costo_dsc', 'Funcionario Encargado_dsc', 'Ubicación del Trabajo/Servicio']
+
+    # Seleccionar una columna para el gráfico de pastel
+    columna_graficar = st.selectbox('Elige una columna para el gráfico de pastel', options=columnas)
+
+    if columna_graficar:
+        valores = df[columna_graficar].value_counts().head(10)
+        fig = px.pie(valores, values=valores, names=valores.index, title=f'Distribución de los 10 más importantes en {columna_graficar}')
+        st.plotly_chart(fig)
+    else:
+        st.warning('No hay columnas disponibles para generar el gráfico de pastel.')
+
+def crear_grafico (tipo,df):
+
+    if (len(tipo)>=0):
+        
+        if tipo == "Barras":
+            grafico_barras(df)
+
+        elif tipo == "Lineas":
+           grafico_lineas(df)
+
+        elif tipo == "BoxPlot":
+           grafico_boxplot(df)
+
+        elif tipo == "Histograma":
+           grafico_histograma(df)
+        
+        elif tipo =="Pastel":
+            grafico_pastel(df)
+
+    else:
+        st.write("Algo Salio mal vuelve a intentarlo")
+
 def principal():
     size_title = 'font-size: 24px; text-align: center; color: #D8E2DC; font-weight: lighter'
     title = "Aplicación para análisis exploratorio y visual de la DSL"
@@ -415,7 +608,7 @@ def principal():
         df = df_mod(df_default)
         dashboard_anual(df)
     else:
-        opciones = st.sidebar.radio("Tipo de análisis", options=["Dashboard Anual","Dashboard Personalizado","Análisis Exploratorio","Gráficos"])    
+        opciones = st.sidebar.radio("Tipo de análisis", options=["Dashboard Anual","Dashboard Personalizado","Análisis Exploratorio","Generador de Gráficos"])    
         bd = cargar_datos(archivo_excel)
         df = df_mod(bd)
 
@@ -423,5 +616,10 @@ def principal():
             dashboard_personalizado(df)
         elif opciones == "Dashboard Anual":
             dashboard_anual(df)
+        elif opciones == "Análisis Exploratorio":
+            analisis_exp(bd)
+        elif opciones == "Generador de Gráficos":
+            tipo_grafico = st.sidebar.selectbox("Seleccione el tipo de grafico", ["Barras","Lineas","BoxPlot","Histograma","Pastel"])
+            crear_grafico(tipo_grafico,bd)
 
 principal()
